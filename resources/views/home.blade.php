19f9b8e88832017edeea5ef011ef3ec1dfe2a3c2
silviafarina29 <silviafarina594@gmail.com>
2022-02-18 14:48:49 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="vieewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> CV SILVIA FARINA | home</title>
+</head>
+
+<body>
+    <h1> halaman home</h1>
+</body>
+
+</html>
